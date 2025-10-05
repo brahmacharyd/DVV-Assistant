6ea9a7c23a5880698c829b5f58a63389ce8e0a2c
Brahmachary Diggoju <brahmacharydiggoju@gmail.com>
2025-10-05 19:24:15 +0530

    hhh

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   apiBaseUrl: '/api',
-  openRouterKey: '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' // optional local testing
+  openRouterKey: '' // optional local testing
 };
